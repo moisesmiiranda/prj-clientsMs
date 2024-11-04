7b24359bb862f74f20376f6118234d3f45c0da31
moisesmiiranda <moisesmiranda1994@gmail.com>
2024-11-04 00:26:58 -0300

    First commit

---
+++ src/main/kotlin/mmiranda/projects/clients_ms/ClientsMsApplication.kt
@@ -0,0 +1,11 @@
+package mmiranda.projects.clients_ms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientsMsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientsMsApplication>(*args)
+}
